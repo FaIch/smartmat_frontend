6bb7e6b00ca8745b55cc0848e1d76e3b585a50b2
Camilla Birkelund <camillabirkelund@hotmail.com>
2023-04-29 17:56:54 +0200

    Functional notification center. Needs styling.

--- src/views/NotificationView.vue
+++ src/views/NotificationView.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="notification-view">
-        <p>Du har {{ numberOfItems }} som snart går ut på dato!</p>
-        <div class="list-of-items" v-for="product in products" :key="product.id">
-            <FridgeItemCardComp :product="product" />
-        </div>
+  <div class="notification-view">
+    <p class="notification-info">Du har {{ numberOfItems }} produkter som snart går ut på dato!</p>
+    <div class="list-of-items" v-for="product in products" :key="product.id">
+      <FridgeItemCardComp :product="product" />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -12,30 +12,15 @@ import axios from 'axios'
 import { onMounted, ref } from 'vue'
 import { FridgeItemCardInterface } from '../components/types'
 import { useUserStore } from '../stores/UserStore'
+import FridgeItemCardComp from '../components/FridgeItemCardComp.vue'
 
-const numberOfItems = ref(0)
-// const itemList: string[] = ['potet', 'gulrot']
+const numberOfItems = ref<number>(0)
 const products = ref<FridgeItemCardInterface[]>([])
 const userStore = useUserStore()
-const updateMessage = ref('')
-
-/* function displayItems() {
-    const listContainer = document.getElementById('list-of-items')
-
-    if (listContainer) {
-        const ul = document.createElement('ul')
-
-        itemList.forEach(item => {
-            const li = document.createElement('li')
-            li.innerText = item
-            ul.appendChild(li)
-        })
-        listContainer.appendChild(ul)
-    }
-} */
 
 async function getItems () {
   const path = 'http://localhost:8080/fridge/get/notification'
+
   const config = {
     headers: {
       'Content-Type': 'application/json'
@@ -47,19 +32,17 @@ async function getItems () {
     .then(async (response) => {
       if (response.status === 200) {
         products.value = response.data
+        numberOfItems.value = products.value.length
       }
     })
     .catch((error) => {
       if (error.response.status === 400) {
-        updateMessage.value = error.response.data.message
-      } else if (error.response.status === 600) {
         userStore.logout()
       }
     })
 }
 
 onMounted(() => {
-  // displayItems()
   getItems()
 })
 
@@ -67,12 +50,12 @@ onMounted(() => {
 
 <style scoped>
 .notification-view {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 2vh;
-    padding: 2rem;
-    background-color: white;
-    border-style: solid;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2vh;
+  padding: 2rem;
+  background-color: white;
+  border-style: solid;
 }
 </style>
