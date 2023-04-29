b3631a60c91829e5636e9d35fee24f0c801d0699
Camilla Birkelund <camillabirkelund@hotmail.com>
2023-04-29 16:48:50 +0200

    Edited method in view

--- src/views/NotificationView.vue
+++ src/views/NotificationView.vue
@@ -1,64 +1,66 @@
 <template>
     <div class="notification-view">
         <p>Du har {{ numberOfItems }} som snart går ut på dato!</p>
-        <div id="list-of-items">
-            <!--Add list of items here-->
+        <div class="list-of-items" v-for="product in products" :key="product.id">
+            <FridgeItemCardComp :product="product" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-// import axios from 'axios'
+import axios from 'axios'
 import { onMounted, ref } from 'vue'
-// import { ItemDisplayInterface } from '../components/types'
+import { FridgeItemCardInterface } from '../components/types'
+import { useUserStore } from '../stores/UserStore'
 
 const numberOfItems = ref(0)
-const itemList: string[] = ['potet', 'gulrot']
+// const itemList: string[] = ['potet', 'gulrot']
+const products = ref<FridgeItemCardInterface[]>([])
+const userStore = useUserStore()
+const updateMessage = ref('')
 
-function displayItems () { // make actual item list instead of the temporary list of strings
-  const listContainer = document.getElementById('list-of-items')
+/* function displayItems() {
+    const listContainer = document.getElementById('list-of-items')
 
-  if (listContainer) {
-    const ul = document.createElement('ul')
+    if (listContainer) {
+        const ul = document.createElement('ul')
 
-    itemList.forEach(item => {
-      const li = document.createElement('li')
-      li.innerText = item
-      ul.appendChild(li)
-    })
-    listContainer.appendChild(ul)
+        itemList.forEach(item => {
+            const li = document.createElement('li')
+            li.innerText = item
+            ul.appendChild(li)
+        })
+        listContainer.appendChild(ul)
+    }
+} */
+
+async function getItems () {
+  const path = 'http://localhost:8080/fridge/get/notification'
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    withCredentials: true
   }
+
+  await axios.get(path, config)
+    .then(async (response) => {
+      if (response.status === 200) {
+        products.value = response.data
+      }
+    })
+    .catch((error) => {
+      if (error.response.status === 400) {
+        updateMessage.value = error.response.data.message
+      } else if (error.response.status === 600) {
+        userStore.logout()
+      }
+    })
 }
 
-// async function getItems() {
-// const path = 'http://localhost:8080/...'
-/**
-    const config = {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        withCredentials: true
-    }
-     */
-/*
-    await axios.get(path, config)
-        .then(async (response) => {
-            if (response.status === 200) {
-                products.value = response.data
-            }
-        })
-        .catch((error) => {
-            if (error.response.status === 400) {
-                updateMessage.value = error.response.data.message
-            } else if (error.response.status === 600) {
-                userStore.logout()
-            }
-        })
-     */
-// }
-
 onMounted(() => {
-  displayItems()
+  // displayItems()
+  getItems()
 })
 
 </script>
