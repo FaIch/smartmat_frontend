9de0fb2b0919e5b99b8cc58bbc018a77b2904335
Camilla Birkelund <camillabirkelund@hotmail.com>
2023-05-01 12:57:29 +0200

    Lint fixed.

--- tests/unit/components/NotificationItemCardComp.spec.js
+++ tests/unit/components/NotificationItemCardComp.spec.js
@@ -3,61 +3,61 @@ import { describe, expect, it } from 'vitest'
 import NotificationItemCardComp from '../../../src/components/NotificationItemCardComp.vue'
 
 describe('NotificationItemCardComp', async () => {
-    it('renders item name', () => {
-        const product = {
-            item: {
-                name: 'Eple',
-                weight: '100'
-            },
-            expirationDate: '2023-04-30',
-            quantity: 1
-        }
+  it('renders item name', () => {
+    const product = {
+      item: {
+        name: 'Eple',
+        weight: '100'
+      },
+      expirationDate: '2023-04-30',
+      quantity: 1
+    }
 
-        const wrapper = mount(NotificationItemCardComp, {
-            props: {
-                product
-            }
-        })
-        expect(wrapper.text()).toContain('Eple')
+    const wrapper = mount(NotificationItemCardComp, {
+      props: {
+        product
+      }
+    })
+    expect(wrapper.text()).toContain('Eple')
+  })
+
+  it('renders item expiration date and quantity', async () => {
+    const product = {
+      item: {
+        name: 'Eple',
+        weight: '100'
+      },
+      expirationDate: '2023-04-30',
+      quantity: 500
+    }
+
+    const wrapper = mount(NotificationItemCardComp, {
+      props: {
+        product
+      }
     })
 
-    it('renders item expiration date and quantity', async () => {
-        const product = {
-            item: {
-                name: 'Eple',
-                weight: '100',
-            },
-            expirationDate: '2023-04-30',
-            quantity: 500
-        }
+    expect(wrapper.text()).toContain('2023-04-30')
+    expect(wrapper.text()).toContain('500')
+  })
 
-        const wrapper = mount(NotificationItemCardComp, {
-            props: {
-                product
-            }
-        })
+  it('shows image when screen width is wider than 640 px', async () => {
+    global.innerWidth = 650
+    const product = {
+      item: {
+        name: 'Eple',
+        weight: '100'
+      },
+      expirationDate: '2023-04-30',
+      quantity: 500
+    }
 
-        expect(wrapper.text()).toContain('2023-04-30')
-        expect(wrapper.text()).toContain('500')
+    const wrapper = mount(NotificationItemCardComp, {
+      props: {
+        product
+      }
     })
 
-    it('shows image when screen width is wider than 640 px', async () => {
-        global.innerWidth = 650
-        const product = {
-            item: {
-                name: 'Eple',
-                weight: '100',
-            },
-            expirationDate: '2023-04-30',
-            quantity: 500
-        }
-
-        const wrapper = mount(NotificationItemCardComp, {
-            props: {
-                product
-            }
-        })
-
-        expect(wrapper.find('.card-image').exists()).toBe(true)
-    })
+    expect(wrapper.find('.card-image').exists()).toBe(true)
+  })
 })
