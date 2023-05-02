61daa51ace3da9cf34bdd3c1ad51577dac307bc1
Joakim <joakimfalch@outlook.com>
2023-05-02 11:07:55 +0200

    Added e2e tests

--- tests/e2e/specs/shoppingList.cy.js
+++ tests/e2e/specs/shoppingList.cy.js
@@ -1,35 +1,33 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
-    return false;
-});
-
-
+  return false
+})
 
 describe('Week Menu', () => {
-    beforeEach(()=>{
-        cy.visit('http://localhost:5000/login')
-        cy.get('[data-cy=input-email]').type('test@test.com')
-        cy.get('[data-cy=input-password]').type('123456789')
-        cy.get('[data-cy=button-login]').click()
-        cy.wait(1000)
-        cy.get('[data-cy=shoppingCart]').click()
-        cy.wait(500)
-    })
+  beforeEach(() => {
+    cy.visit('http://localhost:5000/login')
+    cy.get('[data-cy=input-email]').type('test@test.com')
+    cy.get('[data-cy=input-password]').type('123456789')
+    cy.get('[data-cy=button-login]').click()
+    cy.wait(1000)
+    cy.get('[data-cy=shoppingCart]').click()
+    cy.wait(500)
+  })
 
-    it('Should be able to add an item to shopping list', () => {
-        cy.get('[data-cy=allProducts]').click()
-        cy.wait(500)
-        cy.get('.grid-item').first().click()
-        cy.get('[data-cy=addShopping]').click()
-        cy.wait(500)
-        cy.get('.item-cards').first().click()
-    })
+  it('Should be able to add an item to shopping list', () => {
+    cy.get('[data-cy=allProducts]').click()
+    cy.wait(500)
+    cy.get('.grid-item').first().click()
+    cy.get('[data-cy=addShopping]').click()
+    cy.wait(500)
+    cy.get('.item-cards').first().click()
+  })
 
-    it('Should be able to send shopping list item to fridge', () => {
-        cy.get('.item-cards').first().click()
-        cy.get('.add-button').click()
-        cy.wait(500)
-        cy.get('[data-cy=inventory]').click()
-        cy.wait(1000)
-        cy.get('.item-cards').first().click()
-    })
+  it('Should be able to send shopping list item to fridge', () => {
+    cy.get('.item-cards').first().click()
+    cy.get('.add-button').click()
+    cy.wait(500)
+    cy.get('[data-cy=inventory]').click()
+    cy.wait(1000)
+    cy.get('.item-cards').first().click()
+  })
 })
--- tests/e2e/specs/weekMenu.cy.js
+++ tests/e2e/specs/weekMenu.cy.js
@@ -1,23 +1,21 @@
 Cypress.on('uncaught:exception', (err, runnable) => {
-    return false;
-});
-
-
+  return false
+})
 
 describe('Week Menu', () => {
-    beforeEach(()=>{
-        cy.visit('http://localhost:5000/login')
-        cy.get('[data-cy=input-email]').type('test@test.com')
-        cy.get('[data-cy=input-password]').type('123456789')
-        cy.get('[data-cy=button-login]').click()
-        cy.wait(500);
-    })
+  beforeEach(() => {
+    cy.visit('http://localhost:5000/login')
+    cy.get('[data-cy=input-email]').type('test@test.com')
+    cy.get('[data-cy=input-password]').type('123456789')
+    cy.get('[data-cy=button-login]').click()
+    cy.wait(500)
+  })
 
-    it('Should route to specific week menu', () => {
-        cy.get('[data-cy=weekMenu]').click()
-        cy.wait(500);
-        cy.get('[data-cy=randomMenu]').click()
-        cy.wait(500);
-        cy.url().should('include', '/specificMenu')
-    })
+  it('Should route to specific week menu', () => {
+    cy.get('[data-cy=weekMenu]').click()
+    cy.wait(500)
+    cy.get('[data-cy=randomMenu]').click()
+    cy.wait(500)
+    cy.url().should('include', '/specificMenu')
+  })
 })
